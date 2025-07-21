569fde4de91821598216f0931a1fd6de04c1985f
Virendra Jadhav <virendrajadhav.dev@gmai.com>
2025-07-21 20:37:34 +0530

    Auth Setup on Frontend

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="min-h-screen grid lg:grid-cols-2">
+        <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+            <div className="w-full max-w-md space-y-8">
+            {/* LOGO */}
+            <div className="text-center mb-8">
+                <div className="flex flex-col items-center gap-2 group">
+                <div
+                    className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+                group-hover:bg-primary/20 transition-colors"
+                >
+                    {/* <MessageSquare className="size-6 text-primary" /> */}
+                </div>
+                <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+                <p className="text-base-content/60">Get started with your free account</p>
+                </div>
+                </div>
+            </div>    
+        </div>
+    </div>
   )
 }
 

7fac6c6b8d3773b3cb11b2972f2bdc04dd4b3505
Virendra Jadhav <virendrajadhav.dev@gmai.com>
2025-08-13 22:10:03 +0530

    commit

--- server/src/routes/v1/message.routes.js
+++ server/src/routes/v1/message.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMessages, getUsersForSidebar, sendMessage } from "../../controller/message.controller.js";
+import {getMessages, getUsersForSidebar, sendMessage} from "../../controller/message/message.controller.js"
 
 
 const messageRouter = express.Router();
